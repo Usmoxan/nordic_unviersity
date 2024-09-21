261e5eb749311abadbf300dd4d7ab3d5a07b5984
Usmoxan Tadjibayev <101461756+Usmoxan@users.noreply.github.com>
2024-09-21 17:53:16 +0500

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nordik universiteti',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF296BCF)),
